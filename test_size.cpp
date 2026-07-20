5ee3df0a1ec90f8f00af151c5933d0cddfdada5f
Nischal Khanal <khanalnischal2075@gmail.com>
2026-07-20 12:13:51 +0545

    Remove leftover compile artifacts

--- test_size.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "include/protocol.hpp"
-int main() {
-    std::cout << "OrderbookSnapshot: " << sizeof(OrderbookSnapshot) << std::endl;
-    std::cout << "MemoryStateSnapshot: " << sizeof(MemoryStateSnapshot) << std::endl;
-    return 0;
-}
